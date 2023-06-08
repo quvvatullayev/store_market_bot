bb76cbd26ab5171de21fac4176b9d53d22866aed
ogabek <quvvatullayev@gmail.com>
2023-06-08 09:36:27 +0500

    create updeate user

---
+++ db.py
@@ -0,0 +1,42 @@
+import requests
+import json
+from tinydb import TinyDB, Query
+from tinydb.database import Document
+
+base_url = 'http://storemarket.pythonanywhere.com/shop/'
+
+class DB:
+    def __init__(self, path):
+        self.db = TinyDB(path)
+
+    def get_start(self):
+        request = requests.get(base_url + 'start/')
+        data = request.json()
+        print(data)
+        print(type(data))
+
+    def add_user(self, username, name, chat_id, phone_number=11):
+        user_data = {
+            'username': username,
+            'name': name,
+            'chat_id': chat_id,
+            'phone': phone_number
+        }
+
+        user = requests.post(base_url + 'add-user/', data=user_data)
+        return user.json()
+    
+    def updeate_user(self,chat_id, phone_number):
+        user_data = {
+            'chat_id': chat_id,
+            'phone': phone_number
+        }
+        user = requests.post(base_url + 'update-user/', data=user_data)
+        return user.json()
+
+
+test = DB('db.json')
+# start = test.get_start()
+add_user = test.updeate_user(123456)
+
+print(add_user)
